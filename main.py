6b8a6a006920ce34eb12437ebd8e83770e88090d
naedavis <naeemahndavis@gmail.com>
2021-04-16 12:21:17 +0200

    Added main.py file

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ ave_speed = int(input("Enter Average speed of road: "))
 if current_speed < ave_speed:
     print("okay")
 else:
-    diff = current_speed - ave_speed
-    points = diff / 5
-    print(" Your points are: " + str(points))
+     diff = current_speed - ave_speed
+     points = diff / 5
+     print("Your points are: " + str(points))
+if points >= 12:
+     print("GO TO JAIL!")
 
 
