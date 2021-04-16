6d0dc1cd1a2eda886ea40bc8ef23e3aac6ef9f77
naedavis <naeemahndavis@gmail.com>
2021-04-16 12:09:23 +0200

    Added main.py file.

---
+++ main.py
@@ -0,0 +1,13 @@
+#Determining your points for driving over speed limit
+
+current_speed = int(input("Enter your speed (km/h): "))
+ave_speed = int(input("Enter Average speed of road: "))
+
+if current_speed < ave_speed:
+    print("okay")
+else:
+    diff = current_speed - ave_speed
+    points = diff / 5
+    print(" Your points are: " + str(points))
+
+
